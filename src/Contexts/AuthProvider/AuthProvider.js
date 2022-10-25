ecb5d48fbde690da4412b51d930cc3a94cac8ab3
raju-miah <rajumiahpollob7@gmail.com>
2022-10-26 04:39:38 +0600

    added user logout

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../../firebase/firebase.config';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -25,6 +25,10 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+        return signOut(auth);
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             console.log('user state changed in auth', currentUser);
@@ -35,7 +39,7 @@ const AuthProvider = ({ children }) => {
         }
     }, [])
 
-    const authInfo = { user, singUp, logIn, googleLogin }
+    const authInfo = { user, singUp, logIn, googleLogin, logOut }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,17 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 import './Header.css';
+import { FaUser } from "react-icons/fa";
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
+
+    const handelLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+    }
+
     return (
         <nav className='navbar'>
             <h3 className='nav-logo'>logo</h3>
@@ -17,10 +25,28 @@ const Header = () => {
                 <Link to='/courses'><li>COURSES</li></Link>
                 <Link to='/faq'><li>FAQ</li></Link>
                 <Link to='/blog'><li>BLOG</li></Link>
-                <button className='nav-btn'><Link to='/login'><li>LOGIN</li></Link></button>
-                <button className='nav-btn'><Link to='/signup'><li>SIGN UP</li></Link></button>
                 <button className='nav-btn'><Link to='/dark'><li>DARK</li></Link></button>
-                <li>{user?.displayName}</li>
+
+                {
+                    user?.uid ?
+                        <>
+                            <li>
+                                {user?.displayName}
+                            </li>
+                            <button onClick={handelLogOut}>Logout</button>
+                        </>
+                        :
+                        <>
+                            <button className='nav-btn'><Link to='/login'><li>LOGIN</li></Link></button>
+                            <button className='nav-btn'><Link to='/signup'><li>SIGN UP</li></Link></button>
+                        </>
+                }
+                {
+                    user?.photoURL ?
+                        <img style={{ height: '30px' }} src={user?.photoURL} alt="" />
+                        :
+                        <FaUser />
+                }
             </ul>
         </nav>
     );
