a20c79dc20f63a762b543347c812f6af2b843cbd
raju-miah <rajumiahpollob7@gmail.com>
2022-10-27 03:16:32 +0600

    added tool tip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,32 +21,34 @@ const Header = () => {
                 <h3 className='nav-text'><span className='text-un'>un</span>Courses</h3>
             </div>
             <ul className='nav-links'>
+                <button className='nav-btn'><Link>DARK</Link></button>
                 <Link to='/'><li>HOME</li></Link>
                 <Link to='/courses'><li>COURSES</li></Link>
                 <Link to='/faq'><li>FAQ</li></Link>
                 <Link to='/blog'><li>BLOG</li></Link>
-                <button className='nav-btn'><Link to='/dark'><li>DARK</li></Link></button>
 
                 {
                     user?.uid ?
                         <>
-                            <li>
-                                {user?.displayName}
-                            </li>
-                            <button onClick={handelLogOut}>Logout</button>
+                            {
+                                user?.photoURL ?
+                                    <div className="tooltip">
+                                        <img className='user-img' src={user?.photoURL} alt="" />
+                                        <span className="tooltiptext">{user.displayName
+                                        }</span>
+                                    </div>
+                                    :
+                                    <FaUser />
+                            }
+                            <button onClick={handelLogOut} className='nav-btn'>LOGOUT</button>
                         </>
                         :
                         <>
-                            <button className='nav-btn'><Link to='/login'><li>LOGIN</li></Link></button>
-                            <button className='nav-btn'><Link to='/signup'><li>SIGN UP</li></Link></button>
+                            <button className='nav-btn'><Link to='/login'>LOGIN</Link></button>
+                            <button className='nav-btn'><Link to='/signup'>SIGN UP</Link></button>
                         </>
                 }
-                {
-                    user?.photoURL ?
-                        <img className='user-img' src={user?.photoURL} alt="" />
-                        :
-                        <FaUser />
-                }
+
             </ul>
         </nav>
     );
